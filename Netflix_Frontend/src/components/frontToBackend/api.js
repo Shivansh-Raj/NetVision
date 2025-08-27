046bea1eee082f983b13340f1fc7b93bf04c2c74
Shivansh Raj <shivanshrajdehl@gmail.com>
2025-08-27 21:53:22 +0530

    Database updated

--- Netflix_Frontend/src/components/frontToBackend/api.js
+++ Netflix_Frontend/src/components/frontToBackend/api.js
@@ -1,11 +1,18 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const apiUrl = "/choreo-apis/netvision/netbackend/v1";
+// const apiUrl = "/choreo-apis/netvision/netbackend/v1";
+const apiUrl = "https://b53520bd-d323-4148-9d70-2ac5014cbacb-prod.e1-us-east-azure.choreoapis.dev/netbackend/netbackend/v1.0";
+
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL: apiUrl,
+// });
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL: apiUrl,
+  baseURL: apiUrl || import.meta.env.VITE_API_URL,
 });
+
 
 // Basically I am setting authorization header here since my backend uses JWT authentication method
 api.interceptors.request.use(
